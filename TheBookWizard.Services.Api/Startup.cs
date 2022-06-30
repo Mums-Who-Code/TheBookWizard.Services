d3419eb1eff493b0b252484bbfa3417312444c55
navjot kaur <kaurnavjot1983@outlook.com>
2022-06-30 15:04:17 -0700

    INFRA: Initialize API Project

---
+++ TheBookWizard.Services.Api/Startup.cs
@@ -0,0 +1,55 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c)  MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+using Microsoft.OpenApi.Models;
+
+namespace TheBookWizard.Services.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration) =>
+            Configuration = configuration;
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddSwaggerGen(options =>
+            {
+                var openApiInfo = new OpenApiInfo
+                {
+                    Title = "TheBookWizard.Services.Api",
+                    Version = "v1"
+                };
+
+                options.SwaggerDoc(
+                    name: "v1",
+                    info: openApiInfo);
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint(
+                        url: "/swagger/v1/swagger.json",
+                        name: "TheBookWizard.ServicesApi v1");
+                });
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
+        }
+    }
+}
